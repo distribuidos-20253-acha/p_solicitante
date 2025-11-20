b74762d521f8d960c9d3df4ace109fa6e54e7033
Achalogy <achalogy@gmail.com>
2025-11-19 20:42:45 -0500

    override host & port in ZMQSyncRequest

--- src/net/ZMQSyncRequest.ts
+++ src/net/ZMQSyncRequest.ts
@@ -1,7 +1,9 @@
 import { BaseZMQSyncRequest } from "@acha/distribuidos/zeromq/BaseZMQSyncRequest"
 import { BibOperation, type BibResponse } from "@acha/distribuidos/schemas/BibOperation"
-
+import "dotenv/config"
 export default class ZMQSyncRequest extends BaseZMQSyncRequest {
+  override host = process.env.LOAD_MANAGER_HOST!;
+  override port = process.env.LOAD_MANAGER_PORT!;
 
   override sendRenew(context: { body: BibOperation; }): Promise<BibResponse> {
     return super.resendBody(context)
